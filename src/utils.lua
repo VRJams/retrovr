7ee663430d5a626e0bb2689d8396a6015935324b
Samuel Gosselin <gosselin.sam@gmail.com>
2022-05-30 20:37:52 -0700

    main: fixup the utils.dump() function.

--- src/utils.lua
+++ src/utils.lua
@@ -13,7 +13,7 @@ function utils.dump(o)
       local s = '{ '
       for k,v in pairs(o) do
          if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
+         s = s .. '['..k..'] = ' .. utils.dump(v) .. ','
       end
       return s .. '} '
    else
