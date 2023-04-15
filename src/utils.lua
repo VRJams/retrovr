792e100c239e79c761c022beed6ad106cb9ef029
Udinanon <m.trapanotto@tutamail.com>
2023-04-15 15:49:46 +0200

    Added draw_axes and print_paths functions to utils

--- src/utils.lua
+++ src/utils.lua
@@ -21,8 +21,16 @@ function utils.dump(o)
    end
 end
 
+---Compute worldspace coords of line-plane intersect
+---@param rayPos lovr.Vec3 ray origin
+---@param rayDir lovr.Vec3 ray direction
+---@param planePos lovr.Vec3 plane center
+---@param planeDir lovr.Vec3 plane normal
+---@return lovr.Vec3|nil worldposition position or nil
 function utils.raycast(rayPos, rayDir, planePos, planeDir)
+  
   local dot = rayDir:dot(planeDir)
+  -- if ray fully parallel to plane
   if math.abs(dot) < .001 then
     return nil
   else
@@ -32,6 +40,31 @@ function utils.raycast(rayPos, rayDir, planePos, planeDir)
     else
       return nil
     end
+  end
+end
+
+function utils.drawAxes()
+  lovr.graphics.setColor(1, 0, 0)
+  lovr.graphics.line(0, 0, 0, 1, 0, 0)
+  lovr.graphics.setColor(0, 1, 0)
+  lovr.graphics.line(0, 0, 0, 0, 1, 0)
+  lovr.graphics.setColor(0, 0, 1)
+  lovr.graphics.line(0, 0, 0, 0, 0, 1)
+  lovr.graphics.setColor(1, 1, 1)
+end
+
+function utils.print_paths()
+  ANDROID = lovr.system.getOS() == 'Android'
+  print("ANDROID " .. (ANDROID and 1 or 0))
+
+  print("EXEC PATH " .. lovr.filesystem.getExecutablePath())
+  print("WORK DIR " .. lovr.filesystem.getWorkingDirectory())
+  print("APPDATA DIR " .. lovr.filesystem.getAppdataDirectory())
+  print("SAVE DIR " .. lovr.filesystem.getSaveDirectory())
+  print("SOURCE DIR " .. lovr.filesystem.getSource())
+  if not ANDROID then
+    -- not valid on Android
+    print("USER DIR " .. lovr.filesystem.getUserDirectory())
   end
 end
 
